c94976a2e0993ccf59864a25ab5698f4c995b4d8
rogerniesten <r.niesten@maastrichtuniversity.nl>
2018-11-12 12:14:03 +0100

    first commit [RITDEV-535]

---
+++ README.md
@@ -0,0 +1,10 @@
+Install:
+
+docker build -t dh-slack-cleaner .
+
+Run:
+
+docker run --rm -it dh-slack-cleaner <days-to-retain> <slack-channel> <msg|bot> [commit]
+
+
+# dh-slack-cleaner
---
+++ slack-clean.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+if [[ "$#" -lt 3 ]]; then
+    echo -e "\nSYNTAX: $0 <days-to-retain> <slack-channel> <msg|bot> [commit]\n"
+    exit 1
+fi
+
+if [ -z ${SLACK_TOKEN} ]; then
+    echo "No slack token found! Please set in env var SLACK_TOKEN. Script aborted"
+    exit 1
+fi
+
+RETAIN_DAYS=$1
+SLACK_CHANNEL="$2"
+
+
+RETAIN_DATE=$(date --date="${RETAIN_DAYS} days ago" +%Y%m%d)
+
+ARGS="--channel ${SLACK_CHANNEL} --log "
+
+if [[ "$@" == *"bot"* ]]; then
+    ARGS="${ARGS} --message --bot "
+elif [[ "$@" == *"msg"* ]]; then
+    ARGS="${ARGS} --message --user *"
+else
+    echo "Type (bot/msg) must be specified!"
+    exit 1
+fi
+
+if [[ "$@" == *"commit"* ]]; then
+    ARGS="${ARGS} --rate 1 --Xperform "
+fi
+
+
+echo "=== cleaning slack messages =============================="
+echo "remove before ${RETAIN_DATE}"
+echo "arguments: ${ARGS}"
+echo "----------------------------------------------------------"
+
+#slack-cleaner --help
+echo "slack-cleaner --token ... --before ${RETAIN_DATE} ${ARGS}"
+slack-cleaner --token ${SLACK_TOKEN} --before ${RETAIN_DATE} ${ARGS}
+
+echo "----------------------------------------------------------"
+
+exit 0
